0ea68d3546dc3f976620b7dfcfe949d5f0bcda5f
Themba <mahlangu843@gmail.com>
2026-07-02 14:14:22 +0200

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vertbiz.app',
+  appName: 'vertbiz',
+  webDir: 'dist'
+};
+
+export default config;
